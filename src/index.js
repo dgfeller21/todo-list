dd803198c30f2a6bca0e0bde9c895ffa99e9b64f
Daniel <dgfeller21@gmail.com>
2024-04-10 21:15:55 -0500

    make pretty

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import './style.css'
-import createDialog from './taskModal.js'
-import { displayTasks } from './taskModal.js';
-import {tasks} from './task.js'
-const addTask = document.querySelector("#addTask");
+import "./style.css"
+import createDialog from "./taskModal.js"
+import { displayTasks } from "./taskModal.js"
+import { tasks } from "./task.js"
+const addTask = document.querySelector("#addTask")
 const content = document.querySelector("#content")
 const dialog = createDialog()
 content.append(dialog)
 
-if(localStorage.getItem('myTasks')) {
-    let storageTasks = JSON.parse(localStorage.getItem('myTasks'))
-    for(let item of storageTasks) {
-        tasks.push(item)
-    }
-    displayTasks()
+if (localStorage.getItem("myTasks")) {
+  let storageTasks = JSON.parse(localStorage.getItem("myTasks"))
+  for (let item of storageTasks) {
+    tasks.push(item)
+  }
+  displayTasks()
 }
 
-addTask.addEventListener("click", e => {
-    dialog.showModal()
+addTask.addEventListener("click", (e) => {
+  dialog.showModal()
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|jpeg)$/i,
-                type: 'asset/resource'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 }
